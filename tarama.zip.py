51162f1ad5e44391ed47cccdf1e31ac676e5f66e
MeryemDinc0 <meryem0dinc@icloud.com>
2024-07-10 11:10:30 +0300

    first commit

---
+++ tarama.zip.py
@@ -0,0 +1,48 @@
+Loading:    14s, ETA:   0s [========================>]    8.70M/8.70M sigs      
+Compiling:   3s, ETA:   0s [========================>]       41/41 tasks 
+
+/home/meryem/Desktop/virus_project/virustotal-sorgu/virus.zip: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/README.md: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/virus.zip.py: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/HEAD: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/config: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/pre-push.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/update.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/fsmonitor-watchman.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/commit-msg.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/applypatch-msg.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/post-update.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/prepare-commit-msg.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/pre-applypatch.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/pre-rebase.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/sendemail-validate.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/pre-commit.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/pre-receive.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/push-to-checkout.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/hooks/pre-merge-commit.sample: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/COMMIT_EDITMSG: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/index: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/refs/remotes/origin/main: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/refs/heads/main: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/info/exclude: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/description: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/logs/HEAD: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/logs/refs/remotes/origin/main: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/logs/refs/heads/main: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/objects/cf/2d5358f6743410af7d0fa23b96671c4174a1c2: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/objects/93/44c4ee5292d87959632fc7206de7469f67411f: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/objects/8b/137891791fe96927ad78e64b0aad7bded08bdc: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/objects/65/be18c782dd60437d78ed81ecafdae9931e9fe3: OK
+/home/meryem/Desktop/virus_project/virustotal-sorgu/.git/objects/fa/a85bf5ecd748334c974ec6903426b59b756bf6: OK
+
+----------- SCAN SUMMARY -----------
+Known viruses: 8695846
+Engine version: 1.0.5
+Scanned directories: 23
+Scanned files: 33
+Infected files: 0
+Data scanned: 0.02 MB
+Data read: 0.01 MB (ratio 2.00:1)
+Time: 18.089 sec (0 m 18 s)
+Start Date: 2024:07:10 10:57:06
+End Date:   2024:07:10 10:57:24
